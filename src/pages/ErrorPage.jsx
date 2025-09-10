93ed5b5d8f9de6950f6c8a1a975077b15fd146d0
DanielYuanxk <dyxk1234@hotmail.com>
2025-09-09 21:04:15 -0400

    edit error page text style

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,16 +5,16 @@ const ErrorPage = () => {
   const err = useRouteError();
   if (isRouteErrorResponse(err)) {
     return (
-      <div>
+      <div className="text-2xl font-bold">
         <h1>{err.status}</h1>
         <p>{err.data?.message}</p>
       </div>
     );
   }
   if (err instanceof Error) {
-    return <p>Network error: {err.message}</p>;
+    return <p className="text-2xl font-bold">Network error: {err.message}</p>;
   }
-  return <p>Unknown error</p>;
+  return <p className="text-2xl font-bold">Unknown error</p>;
 };
 
 export default ErrorPage;
